c780418aa83b904067c339bc878086965dbf3194
Syed Razvi <103219618@swin.edu.au>
2020-08-23 21:23:52 +1000

    starting unit tests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -9,11 +9,13 @@ namespace Tests
         // Write Unit Tests for all functions for your assigned group
 
         // 1.  Method receives the two sides of a Rectangle and return the perimeter
+        [Theory]
+
 
         // 2.  Checks if a number is a triangular number
 
         // 3.  Coverts a temperature from Farenheit to Celcius, rounded up to the nearest whole number
-        
+        vcd 
         // 4.  Checks a string for either leading or trailing white spaces
 
         // 5.  Takes a string and makes every odd letter uppercase and every even letter lowercase
