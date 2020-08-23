d7ce04180bae705c2d387646449ed1907a9ca872
Syed Razvi <103219618@swin.edu.au>
2020-08-23 21:14:28 +1000

    group2

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -7,5 +7,18 @@ namespace Tests
     public class MiscLibTests
     {
         // Write Unit Tests for all functions for your assigned group
+
+        // 1.  Method receives the two sides of a Rectangle and return the perimeter
+
+        // 2.  Checks if a number is a triangular number
+
+        // 3.  Coverts a temperature from Farenheit to Celcius, rounded up to the nearest whole number
+        
+        // 4.  Checks a string for either leading or trailing white spaces
+
+        // 5.  Takes a string and makes every odd letter uppercase and every even letter lowercase
+
+        // 6.  Counts the number of digits found in a string
+
     }
 }
