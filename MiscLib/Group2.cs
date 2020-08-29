51ee8c02962c554a85513b17fa6824aa636f90aa
Syed Razvi <103219618@swin.edu.au>
2020-08-29 22:50:02 +1000

    1and2

--- MiscLib/Group2.cs
+++ MiscLib/Group2.cs
@@ -5,30 +5,44 @@ namespace MiscLib
     public class GroupTwoFunctions
     {
         //---- Group 2 Functions ----------
-        public int CalcRectPerimeter(int length, int width) {
+        public int CalcRectPerimeter(int length, int width)
+        {
+            return (length + width) * 2;
+        }
+
+        public bool IsTriangularNum(int num)
+        {
+            long calc_num = 8 * num + 1;
+            long t = (long)Math.Sqrt(calc_num);
+
+            if (t * t == calc_num)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public int FarenheitToCelcius(int farenheit)
+        {
             throw new NotImplementedException();
         }
 
-        public bool IsTriangularNum(int num) {
+        public bool CheckWhiteSpace(string word)
+        {
             throw new NotImplementedException();
         }
 
-        public int FarenheitToCelcius(int farenheit) {
+        public string CamelString(string word)
+        {
             throw new NotImplementedException();
         }
 
-        public bool CheckWhiteSpace(string word) {
+        public int CountDigits(string word)
+        {
             throw new NotImplementedException();
         }
 
-        public string CamelString(string word) {
-            throw new NotImplementedException();
-        }
 
-        public int CountDigits(string word) {
-            throw new NotImplementedException();
-        }
-
-        
     }
 }
